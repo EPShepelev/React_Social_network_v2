5bc4a27e4c7f3aedb340abfae3b870d68ea4358a
Шепелев Евгений <shepelev.e@ad1team.ru>
2022-02-14 17:33:50 +0300

    clear public folder

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import logo from '../../assets/images/logo.png'
+import './Header.scss'
+
 
 export const Header: React.FC = () => {
   return (
-    <div>
-      <img src={logo} alt='logo'></img>
+    <div className='header'>
+      <img className='logo' src={logo} alt='logo'></img>
       Header
     </div>
   );
